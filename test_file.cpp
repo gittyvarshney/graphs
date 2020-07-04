175974c72657c92fec4c4a506e0df9ad11a5fa4d
gittyvarshney <pv66645@gmail.com>
2020-07-04 19:12:54 +0530

    commited from atom

---
+++ test_file.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  #ifndef ONLINE_JUDGE
+    freopen("D:\\Project sunrise\\input.txt", "r", stdin);
+    freopen("D:\\Project sunrise\\output.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cout << "hello world \n";
+}
